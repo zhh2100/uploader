cad875a825c899250b6faf5ef90ef294c8263668
张洪华 <112738102@qq.com>
2016-11-26 18:42:06 +0800

    开始

---
+++ README.md
@@ -0,0 +1,4 @@
+<pre>
+https://github.com/qqtxt/jquery.more.js
+
+</pre>
---
+++ uploader.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_FILES['myFile'])) {
+	//扩展名
+	$ext=strtolower(substr($_FILES['myFile']['name'],strrpos($_FILES['myFile']['name'],'.')));
+	//唯一临时文件名
+	if(in_array($ext,array('.jpg','.png','.gif'))){
+		list($usec, $sec) = explode(" ", microtime());
+		$name=$sec.intval($usec*10000000).$ext;
+		if(move_uploaded_file($_FILES['myFile']['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/'.$name)){
+			echo $name;
+		}
+	}
+}
+echo '';
